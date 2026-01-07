7164e0b9c39dfe91f0731619137262aec1ff17e9
Mimi Sajeev <1893@trenser.com>
2026-01-07 10:18:47 +0530

    FreeRTOS Project

---
+++ Src/common.h
@@ -0,0 +1,20 @@
+/*
+ * common.h
+ *
+ *  Created on: Jan 5, 2026
+ *      Author: 1893
+ */
+
+#ifndef SRC_COMMON_H_
+#define SRC_COMMON_H_
+
+#include <stdint.h>
+
+typedef struct
+{
+	int16_t nTemperature;
+	uint32_t ulTimestamp;
+}TEMPERATURE_DATA_t;
+
+
+#endif /* SRC_COMMON_H_ */
